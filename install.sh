bdc396e90d92d89f5aa5655c3dd4f436b6d02027
Pedro Lacerda <kanvuanza@gmail.com>
2012-10-04 01:24:24 -0300

    ...

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x
 ls -1 | grep -v ${0##./} | xargs -i sh -c "rm $HOME/.{}; ln -s $PWD/{} $HOME/.{}"
 git clone 'http://github.com/gmarik/vundle' vim/bundle/vundle
 vim -c BundleInstall -c q -c q
