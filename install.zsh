d74eb9dfdacfd8541dc5f30f196a8a01f833e638
Pedro Lacerda <kanvuanza@gmail.com>
2012-10-18 04:29:14 -0300

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -4,5 +4,6 @@ setopt extended_glob
 git clone 'http://github.com/kanvuanza/dotfiles' dotfiles
 git clone 'http://github.com/gmarik/vundle' dotfiles/vim/bundle/vundle
 
+cd dotfiles
 for f in ^${0:t}*; rm -r ~/.$f && ln -s $PWD/$f ~/.$f
 vim -c BundleInstall -c q -c q
