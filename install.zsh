de7691c5c77968090889ad7b632e1ded933f345d
Pedro Lacerda <kanvuanza@gmail.com>
2012-10-18 04:30:21 -0300

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -5,5 +5,5 @@ git clone 'http://github.com/kanvuanza/dotfiles' dotfiles
 git clone 'http://github.com/gmarik/vundle' dotfiles/vim/bundle/vundle
 
 cd dotfiles
-for f in ^${install.zsh:t}*; rm -r ~/.$f && ln -s $PWD/$f ~/.$f
+for f in ^${install\.zsh:t}*; rm -r ~/.$f && ln -s $PWD/$f ~/.$f
 vim -c BundleInstall -c q -c q
