dec15d15f6144515baa500f292f89625e955566a
Pedro Lacerda <kanvuanza@gmail.com>
2013-03-13 11:12:13 -0300

    \

--- install.zsh
+++ install.zsh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 setopt extended_glob
 
-for f in ^${0:t}*; rm -fr ~/.$f && ln -s $PWD/$f ~/.$f
 git clone 'http://github.com/gmarik/vundle' vim/bundle/vundle
+git clone 'https://github.com/zsh-users/antigen.git' ~/.antigen
+
+for f in ^${0:t}*; rm -fr ~/.$f && ln -s $PWD/$f ~/.$f
+
 vim -c BundleInstall -c q -c q
+zsh ~/.zshrc
